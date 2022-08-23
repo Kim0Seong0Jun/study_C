e32f8c6b1c8ad64b8206f9a5325e39ddffc77533
Kim0Seong0Jun <105624059+Kim0Seong0Jun@users.noreply.github.com>
2022-08-23 14:28:14 +0900

    .
    .

--- day2/array_prepar.c
+++ day2/array_prepar.c
@@ -85,14 +85,49 @@ int main (void)
         printf("\n");
     } */
 
-    for (int i = 1; i <= 10; i++)
+    /* for(int i = 0; i < 5; i++)
     {
-        for(int j =1; j <= i; j++)
-        {
+            for(int j = 0; j <= i; j++)
+            {
             printf("*");
+            }
+        printf("\n");
+    } */
+
+   /*  printf("1 ~ 10까지 짝수를 출력합니다!! \n");
+   for(int i = 1; i <= 10; i++)
+   {
+    if(i % 2 == 0)
+    printf("%d\n", i);
+   } */
+
+   /* printf("1 ~ 10 까지 홀수를 출력합니다!! \n");
+   for(int i = 1; i <= 10; i++)
+   {
+    if (i % 2 == 1)
+    printf("%d\n", i);
+   } */
+
+            // 예제 4번
+   /* for(int i = 1; i < 10; i++)
+   {
+        for(int j = 1; j < 10; j++)
+        {
+            printf("%d\t", i * j);
         }
         printf("\n");
-    }
+   } */
+
+            // 예제 5번
+    int num, sum = 0;
+    printf("시험 점수를 입력하세요 (0이면 종료) \n");
+    do
+    {
+        scanf("%d", &num);
+        sum += num;
+    } while (num > 0);
+    
+    printf("시험 점수의 합계는 %d 입니다. \n", sum);
 
     return 0;
 }
